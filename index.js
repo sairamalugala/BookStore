b5993c03561f014acb8a32346f0cdfffc668ce30
sairamalugala <algualasairam@gmail.com>
2021-11-10 06:31:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
+app.get('/', (req, res)=>{
+    res.render('index.ejs')
+});
+
+var books = [];
+
+app.get('/books', (req, res)=>{
+    res.status(200).send(books)
+})
+
+app.post('/books/new', (req,res)=>{
+    const book = {
+        '_id':Math.random().toString(16).substring(2),
+        ...req.body
+    }
+    books.push(book);
+    res.send(books);
+});
+
+app.delete('/books/:id', (req, res)=>{debugger;
+    const id = req.params.id;
+    books = books.filter((book)=>{
+        return book._id!==id;
+    });
+    res.status(200).send();
+})
+http.createServer(app).listen(3400, ()=>{
+    console.log('server started');
+})
