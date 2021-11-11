e67a0cd55e7ba196209587d07d8a269629cb815c
sairamalugala <algualasairam@gmail.com>
2021-11-11 09:03:27 +0530

    second commit

--- index.js
+++ index.js
@@ -7,32 +7,46 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded());
 
 app.use(bodyParser.json());
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.render('index.ejs')
 });
 
 var books = [];
 
-app.get('/books', (req, res)=>{
+app.get('/books', (req, res) => {
     res.status(200).send(books)
 })
 
-app.post('/books/new', (req,res)=>{
+app.post('/books/new', (req, res) => {
     const book = {
-        '_id':Math.random().toString(16).substring(2),
+        '_id': Math.random().toString(16).substring(2),
         ...req.body
     }
     books.push(book);
     res.send(books);
 });
 
-app.delete('/books/:id', (req, res)=>{debugger;
+app.delete('/books/:id', (req, res) => {
     const id = req.params.id;
-    books = books.filter((book)=>{
-        return book._id!==id;
+    books = books.filter((book) => {
+        return book._id !== id;
     });
     res.status(200).send();
-})
-http.createServer(app).listen(3400, ()=>{
+});
+app.put('/books/:id', (req, res) => {
+    const id = req.params.id;
+    books.every((book, index) => {
+        if (book._id == id) {
+            books[index] = {
+                '_id': id,
+                ...req.body
+            }
+            return false;
+        }
+        return true;
+    })
+    res.status(200).send();
+});
+http.createServer(app).listen(3400, () => {
     console.log('server started');
 })
